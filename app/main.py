e53e76399e8f8a3ef03d08920c9f6c00ee533fbc
Tadeas Uhlir <tadeas.uhlir@gmail.com>
2021-05-06 17:09:00 +0200

    Implement basic functionality of API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from typing import List
 
-from app.schemas import Post, PostFull
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from app.schemas import Post, PostNew, PostEdit
 from app.models import PostModel
 from app.database import session_connect
 
@@ -8,32 +11,38 @@ app = FastAPI()
 db_session = session_connect()
 
 
-@app.get("/")
-def root():
-    """Something like a doc maybe?"""
-    return {"message": "Hello World"}
-
-
-@app.post("/blog/posts/", response_model=PostFull)
-def create_post(new_post: Post):
+@app.post("/blog/posts", response_model=Post)
+def create_post(new_post: PostNew):
     """Create a new blog post"""
-    pass
+    new_post_model = PostModel.new(**new_post.dict(exclude_unset=True))
+    db_session.add(new_post_model)
+    return new_post_model
 
 
-def edit_post():
-    pass
-
-
-def delete_post():
-    pass
-
-
-@app.get("/blog/posts")
+@app.get("/blog/posts", response_model=List[Post])
 def list_posts():
-    pass
+    return db_session.get_all(PostModel)
 
 
-@app.get("/blog/posts/{post_id}", response_model=PostFull)
+@app.put("/blog/{post_id}", response_model=Post)
+def edit_post(post_id: int, post_edits: PostEdit):
+    post_edits_model = PostModel(**post_edits.dict(exclude_unset=True))
+    db_session.update(post_edits_model, post_id)
+    return db_session.get(PostModel, post_id)
+
+
+@app.delete("/blog/posts/{post_id}", response_model=Post)
+def delete_post(post_id: int):
+    return db_session.delete(PostModel, post_id)
+
+
+@app.get("/blog/posts/{post_id}", response_model=Post)
 def post_details(post_id: int):
     """Get details of a blog post"""
-    pass
+    return db_session.get(PostModel, post_id)
+
+
+@app.get("/")
+def index():
+    """Redirect to docs"""
+    return RedirectResponse("/docs")
--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,41 @@
+from typing import Optional
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
-class Post(BaseModel):
+class PostRaw(BaseModel):
+    """Base model for post which contains all possible fields"""
+    title: Optional[str]
+    author: Optional[str]
+    content: Optional[str]
+    id: Optional[int]
+    timestamp_utc: Optional[datetime]
+
+
+class PostEdit(PostRaw):
+    """
+    Not sure if there is more elegant solution, but this schema
+    is necessary to ensure that by PUT edit requests don't try to modify
+    id of a post
+    """
+    @validator('id')
+    def id_unset(cls, v):
+        assert v is None, "Id cannot be editted"
+
+
+class PostNew(PostRaw):
+    """For new posts title, author, and content are mandatory"""
     title: str
     author: str
     content: str
 
 
-class PostFull(Post):
+class Post(PostNew):
+    """
+    This is the Post schema corresponding to PostModel,
+    which is how posts are stored in the FakeDb
+    """
     id: int
     timestamp_utc: datetime
 
